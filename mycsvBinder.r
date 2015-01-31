d0fe4947bdb72374d1596aae0d59d43486e443a5
Saul-R <estifjarris@gmai.com>
2015-01-31 20:20:17 +0000

    test change on mycsvBinder

--- mycsvBinder.r
+++ mycsvBinder.r
@@ -2,7 +2,7 @@ mycsvBinder <- function(directory) {
   savewd<-getwd()
   setwd(paste(getwd(),"/",directory,sep=""))
   filenames <- list.files(path = getwd())
-  bindedCsv <- do.call(rbind,lapply(filenames, read.csv, header = TRUE))
+  bindedCsv <- do.call(rbind,lapply(filenames, read.csv, header = TRUE)) #the heavy stuff
   setwd(savewd)
-  bindedCsv
+  bindedCsv #still trying to figure out how R functions return values, this works, but it might be cheap & dirty
 }
