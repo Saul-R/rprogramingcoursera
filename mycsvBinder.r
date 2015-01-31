e7046e3dd50e874dddceaf60833e5dc4642fb98e
Saul-R <estifjarris@gmai.com>
2015-01-31 21:06:49 +0000

    Removed old files

--- mycsvBinder.r
+++
@@ -1,8 +0,0 @@
-mycsvBinder <- function(directory) {
-  savewd<-getwd()
-  setwd(paste(getwd(),"/",directory,sep=""))
-  filenames <- list.files(path = getwd())
-  bindedCsv <- do.call(rbind,lapply(filenames, read.csv, header = TRUE)) #the heavy stuff
-  setwd(savewd)
-  bindedCsv #still trying to figure out how R functions return values, this works, but it might be cheap & dirty
-}
